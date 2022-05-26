fe807770897453abe1c844445cb6b273e1dc06d4
Boopathy0226 <boopathysaravanan97@gmail.com>
2022-05-26 17:40:26 +0530

    Updated new code

--- src/test/java/com/vtr/GitPractice.java
+++ src/test/java/com/vtr/GitPractice.java
@@ -1,5 +1,36 @@
 package com.vtr;
 
 public class GitPractice {
+	private void method1() {
+		System.out.println("1");
+	}
+
+	private void method2() {
+		System.out.println("2");
+	}
+
+	private void method3() {
+		System.out.println("3");
+	}
+
+	private void method4() {
+		System.out.println("4");
+	}
+
+	private void method5() {
+		System.out.println("5");
+	}
+
+	private void method6() {
+		System.out.println("6");
+	}
+
+	private void method7() {
+		System.out.println("7");
+	}
+
+	private void method8() {
+		System.out.println("8");
+	}
 
 }
